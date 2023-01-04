559ab12207bb2fcc74806b4064fbfc92ea337e3c
Aliaksandr Bandziuk <62721784+aliaksandr-bandziuk@users.noreply.github.com>
2023-01-04 12:01:46 +0100

    fix: made first edits

--- scripts/swiper-recipes.js
+++ scripts/swiper-recipes.js
@@ -3,20 +3,27 @@ const swiper = new Swiper('.swiper-recipes', {
   loop: true,
   slidesPerView: 2,
   spaceBetween: 17,
+  // initialSlide: 4,
+  
 
   breakpoints: {
     // when window width is >= 480px
     480: {
-      slidesPerView: 2.3,
-      spaceBetween: 20
+      slidesPerView: 2.6,
+      spaceBetween: 19,
+      // width: 393,
+    },
+    1424: {
+      slidesPerView: 2.6,
+      spaceBetween: 19,
     },
     1700: {
       slidesPerView: 3.3,
-      spaceBetween: 20
+      spaceBetween: 19
     },
     1920: {
       slidesPerView: 4.3,
-      spaceBetween: 20
+      spaceBetween: 19
     }
   },
 
